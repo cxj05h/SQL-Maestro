c63bf15c0e516589f48052e51ebc16664d9a3baa
cxj05h <cxj05h@acu.edu>
2025-09-12 15:50:28 +0300

    Initial Commit

---
+++ SQLMaestro/SQLMaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLMaestroApp.swift
+//  SQLMaestro
+//
+//  Created by Chris Jones on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SQLMaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SQLMaestroTests/SQLMaestroTests.swift
@@ -0,0 +1,16 @@
+//
+//  SQLMaestroTests.swift
+//  SQLMaestroTests
+//
+//  Created by Chris Jones on 12/09/2025.
+//
+
+import Testing
+
+struct SQLMaestroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
